92b603e131e8a52a1ab4725b5cc08cf4ed8e2ef7
Orçun Çolak <orcun.colak@hazelcast.com>
2024-01-24 12:02:36 +0300

    find extensions example

---
+++ extension/find-extesion.sql
@@ -0,0 +1,6 @@
+-- https://medium.com/@murat.bilal/handy-postgresql-commands-part-1-2f7ea21c29ee
+SELECT * FROM pg_extension;
+  oid  | extname | extowner | extnamespace | extrelocatable | extversion | extconfig | extcondition
+-------+---------+----------+--------------+----------------+------------+-----------+--------------
+ 13564 | plpgsql |       10 |           11 | f              | 1.0        |           |
+(1 row)
