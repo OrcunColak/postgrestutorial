17f6fe966f01f8e647648b11255a7207e8d2ada2
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-19 08:49:10 +0300

    JSONB_BUILD_OBJECT examples

--- columntypes/json/json-operators/get-json-field/get-json-field-test.sql
+++ columntypes/json/json-operators/get-json-field/get-json-field-test.sql
@@ -27,3 +27,16 @@ select product_details ->> 'color' as color from sales_order_detail_demo;
 -- 2. Select array field
 select product_details -> 'size_and_weight' as size_and_weight_json, product_details ->> 'size_and_weight' as size_and_weight_txt
 from sales_order_detail_demo;
+
+-- json_agg : This function aggregates JSON values into a JSON array
+-- Yani tek json nesnesi yerine json array döndürür
+-- https://medium.com/@gunjit29/postgresql-and-json-lets-learn-how-to-use-json-data-in-postgresql-bf2431588406
+SELECT json_agg(product_details) FROM sales_order_detail_demo;
+
+-- jsonb_set : This function updates a JSON object field with a new value.
+UPDATE sales_order_detail_demo SET product_details = jsonb_set(product_details, '{list_price}', '0.0')
+
+-- JSONB_BUILD_OBJECT : JSONB_BUILD_OBJECT function converts a plain text record to JSON data.
+-- https://medium.com/@gunjit29/postgresql-and-json-lets-learn-how-to-use-json-data-in-postgresql-bf2431588406
+INSERT INTO sales_order_detail_demo (product_details) VALUES(JSONB_BUILD_OBJECT('Tired','Everybody is annoying today', 'Hungry','Cannot wait to go home'));
+
