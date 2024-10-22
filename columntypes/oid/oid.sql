5f19b2296b6cfaec2b5fcafffa51e1ec2da938bb
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-22 10:29:31 +0300

    oid example

--- columntypes/oid/oid.sql
+++ columntypes/oid/oid.sql
@@ -1,4 +1,7 @@
 -- See https://blog.devops.dev/how-to-remove-orphan-large-objects-in-postgresql-35d343bb069b
+-- Binary Large Objects (BLOBS) in PostgreSQL should be removed using the lo_unlink() function.
+-- Simply deleting the related row without calling lo_unlink() will leave orphaned records behind.
+-- In such cases, vacuumlo can come to the rescue to clean up these orphaned large objects.
 
 -- Create table to store large object references
 CREATE TABLE files (id SERIAL PRIMARY KEY,filename TEXT, file_oid OID );
